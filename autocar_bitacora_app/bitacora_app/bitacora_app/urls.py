4bbf73357e1276699144a55023d923c9b896ed98
Grynbox <111037791+Grynbox@users.noreply.github.com>
2024-11-19 17:17:24 -0500

    Delete autocar_bitacora_app/bitacora_app directory

--- autocar_bitacora_app/bitacora_app/bitacora_app/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path , include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path("reporte_operaciones/", include("reporte_operaciones.urls")),
-    path('accounts/', include('django.contrib.auth.urls')),  # También incluye las URLs de autenticación
-]
